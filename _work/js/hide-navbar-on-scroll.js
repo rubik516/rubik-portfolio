58b1d9a8eb55a92ec8fa3f8e88d5f55649d608b8
rubik <hathungn614@gmail.com>
2021-12-06 15:24:40 -0800

    Remove console.log debugging messages

--- _work/js/hide-navbar-on-scroll.js
+++ _work/js/hide-navbar-on-scroll.js
@@ -5,23 +5,18 @@ window.onresize = hideNavBarOnScroll;
 
 function hideNavBarOnScroll() {
     if (window.matchMedia('(min-width: 120rem)').matches) {
-        console.log("min 120");
         hideNavBar("5.15rem");
     } else if (window.matchMedia('(min-width: 66.25rem)').matches &&
                 window.matchMedia('(max-width: 120rem)').matches) {
-        console.log("min 66.25, max 120");
         hideNavBar("4.9rem");
     } else if (window.matchMedia('(min-width: 52rem)').matches &&
                 window.matchMedia('(max-width: 66.25rem)').matches) {
-        console.log("min 52, max 66.25");
         hideNavBar("4.7rem");
     } else if (window.matchMedia('(min-width: 26rem)').matches &&
                 window.matchMedia('(max-width: 52rem)').matches) {
-        console.log("min 26, max 52");
         hideNavBar("4.15rem");
     } else {
         // The same case as window.matchMedia('(min-width: 20rem)').matches)
-        console.log("min 20");
         hideNavBar("8.3rem");
     }
 }
